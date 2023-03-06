bcdef4d3463002e2c44fe31feec07299fc3fff27
Joaomachado19 <joaovwmachado@gmail.com>
2023-03-06 16:46:49 -0300

    Atualização 4

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,31 @@
-
+class Ator
+{
+    constructor(id,nome){
+        this.nome=nome;
+        this.id=id;
+    }
+}
+class Diretor
+{
+    constructor(id,nome){
+        this.nome=nome;
+        this.id=id;
+    }
+}
+class Filme
+{
+    constructor(id,titulo,ano,genero,duracao,sinopse,cartaz,direcao,elenco,classificacao,avaliacao){
+        this.id=id;
+        this.titulo=titulo;
+        this.ano=ano;
+        this.genero=genero;
+        this.duracao=duracao;
+        this.sinopse=sinopse;
+        this.cartaz=cartaz;
+        this.direcao=direcao;
+        this.elenco=elenco;
+        this.classificacao=classificacao;
+        this.avaliacao=avaliacao;
+    }
+}
+fetch('http://www.omdbapi.com/?i=tt3896198&apikey=bb1d4839');

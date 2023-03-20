fa9bd346e3a4d718442ed98c8a97b46a23ba7de6
Joaomachado19 <joaovwmachado@gmail.com>
2023-03-20 14:01:28 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -8,13 +8,13 @@ btnBuscarFilme.onclick  = () => {
         .then((resp) => {
             resp.Search.forEach((item)=>{
                 let filme = new Filme(
-                    item.imdbId,
+                    item.imdbID,
                     item.Title,
                     item.Year,
                     null,
-                    item.Genero,
-                    item.Poster,
                     null,
+                    null,
+                    item.Poster,
                     null,
                     null,
                     null,
@@ -33,41 +33,78 @@ btnBuscarFilme.onclick  = () => {
 
 
 
+
+
+
+
+
 let listarFilmes = async (filmes) => {
+
    let listaFilmes = await document.querySelector("#lista-filmes");
+
    listaFilmes.innerHTML = "";
+
    if(filmes.length > 0){
+
     filmes.forEach(async(filme) =>{
+
         console.log(filme);
+
         listaFilmes.appendChild(await filme.getCard());
+
         filme.getBtnDetalhes().onclick=()=>{
+
             detalhesFilme(filme.id);
+
         }
+
     });
+
    }
+
 }
 
+
+
+
 let detalhesFilme = async (id) =>{
+
     fetch("http://www.omdbapi.com/?apikey=bb1d4839&i="+id)
+
     .then((resp)=> resp.json())
+
     .then((resp) => {
+
         console.log(resp);
+
         let filme = new Filme(
+
             resp.imdbID,
+
             resp.Title,
+
             resp.Year,
+
             resp.Genre.split(","),
+
             resp.Runtime,
+
             resp.Poster,
+
             resp.plot,
+
             resp.Director,
+
             resp.Actors.split(","),
+
             resp.awards,
+
             resp.imdbRating
+
         )
+
         console.log(filme)
+
     })
+
 }
-fecharDetalhes.addEventListener ("click",function(){
-    document.getElementById("mostrar-filmes").style.display = "none";
-})
